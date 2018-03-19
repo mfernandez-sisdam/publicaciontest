5ade2b833a493c5d0acb49f364272bd04ec44bcc
margfernan <margfernan@gmail.com>
2018-03-19 17:22:11 -0300

    test

---
+++ src/main/java/com/sisdam/mgf/App.java
@@ -0,0 +1,13 @@
+package com.sisdam.mgf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
